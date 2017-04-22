f5f347c2e4a467c709f23d6cb423749ff98316e3
Anthony Mills <amills@emailmail.net>
2017-04-22 15:46:21 +1000

    Created a cockroach to act as out main character

--- src/js/states/main-menu.js
+++ src/js/states/main-menu.js
@@ -1,12 +1,25 @@
 
 ludumDare.MainMenu = function (game) {
-
+	this.debugFps = true;
 };
 
 ludumDare.MainMenu.prototype = {
 
 	create: function () {
-	
+		if (this.debugFps) {
+			this.game.time.advancedTiming = true;	
+		}
+
+		var textStyle = { 
+						font: "32px Arial", 
+						fill: "#ff0044", 
+						align: "center", 
+						backgroundColor: "#ffff00" 
+					};
+
+		this.gameTitle = ludumDare.phaser.add.text(0, 50, "Ludum Dare #38", textStyle);
+		this.gameTitle.anchor.set(0.5);	
+		this.gameTitle.x = (this.camera.width * 0.5 - (this.gameTitle.width * 0.5));	
 	},
 
 	update: function () {
@@ -14,16 +27,19 @@ ludumDare.MainMenu.prototype = {
 
 	render: function()
 	{
-	  this.game.debug.text('render FPS: ' + (this.game.time.fps || '--') , 2, 14, "#00ff00");
+		if (this.debugFps) {
+			this.game.debug.text('render FPS: ' + (this.game.time.fps || '--') , 2, 14, "#00ff00");			
+		}
+
 	},
 
 	startGame: function (pointer) { 
 
-	    this.camera.fade('#000000');
+		this.camera.fade('#000000');
 
-	    this.camera.onFadeComplete.add(function() {
-	      // Add the next game state in here
-	    },this);		
+		this.camera.onFadeComplete.add(function() {
+		  // Add the next game state in here
+		},this);		
 
 	}
 };
